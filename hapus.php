3d5ec5cdd64399f5366c447cf47189baaa37b937
rianinajaa <fathanaprian73@gmail.com>
2025-09-15 10:17:36 +0700

    hapus ye

--- hapus.php
+++ hapus.php
@@ -1,6 +1,22 @@
 <?php 
 
+include "koneksi.php";
 
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+
+    $sql = "DELETE FROM siswa WHERE id = '$id'";
+    $query = mysqli_query($conn, $sql);
+
+    if($conn->query($sql) === TRUE){
+        echo "Data berhasil di hapus";
+        echo "<br>";
+        echo "<a href='index.php'>kembali</a>";
+    }
+    else{
+        echo "data gagal dihapus";
+    }
+}
 
 
 
